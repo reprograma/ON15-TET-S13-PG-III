af71379516997b9fc515e9baac4ef10bf08c857d
elizapimentel <elizapimentel@hotmail.com>
2022-06-18 16:50:01 -0300

    :tada: Commit Inicial: instalação de dependencias e primeiros códigos

---
+++ src/routes/noteRoutes.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../controller/noteController");
+
+router.post("/create", controller.createNote)
+router.get("/all", controller.getAll)
+router.put("/update/:id", controller.updateNotes)
+router.delete("/delete/:id", controller.deleteNote)
+
+
+module.exports = router
