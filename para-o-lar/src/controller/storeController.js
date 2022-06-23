b494ff412243382ce16c8377ac889cc72b740384
elizapimentel <elizapimentel@hotmail.com>
2022-06-23 16:00:54 -0300

    :sparkles: Feat: including Create

--- para-o-lar/src/controller/storeController.js
+++ para-o-lar/src/controller/storeController.js
@@ -11,9 +11,34 @@ const findAll = async(req, res) => {
     }
 }
 
+const createStore = async(req, res) => {
+    const { review, store, category, neighborhood, address, number, paymment, site } = req.body
+    if(!req.body.store || !req.body.address){
+        return res.status(404).send({
+            "message": "Campo obrigatório",
+            "satusCode": 404
+        })        
+    }
+        const storeExists = StoreSchema.find({ store: req.body.store})
+    if(storeExists) {
+       return res.status(400).send({
+            "message": "Estabelecimento já cadastrado"
+        })
+    }
+    try {
+        const newStore = new StoreSchema ({ review, store, category, neighborhood, address, number, paymment, site, createdAt: new Date() })        
+        const savedStore = await newStore.save()        
+        res.status(201).json(savedStore)        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: error.message})
+    }
+}
+
+
 module.exports = {
     findAll,
-    // createStore,
+    createStore,
     // getById,
     // findSome,
     // updateStore,
--- para-o-lar/src/routes/storeRoutes.js
+++ para-o-lar/src/routes/storeRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const controller = require("../controller/storeController")
 
 router.get("/all", controller.findAll)
-// router.post("/create", controller.createStore)
+router.post("/create", controller.createStore)
 // router.get("/find_some", controller.findSome)
 // router.get("/id/:id", controller.getById)
 // router.put("/update/:id", controller.updateStore)
