f7c711b61536f609518a3d95532e8d93b8670c82
BrunaCelestino <brunaccelestino@gmail.com>
2022-06-23 23:12:18 -0300

    :boom: fix: Resolvendo conflitos com o repositório original

--- aula/server.js
+++ aula/server.js
@@ -1,8 +1,4 @@
 const app = require('./src/app');
 const PORT = process.env.PORT;
-<<<<<<< HEAD:aula/server.js
-=======
-// 3000, 3030, 6000, 6060, 8000, 8080
->>>>>>> a50a0073bcbd3b1eb75a8724e0f52a5eceb0d5ea:server.js
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
