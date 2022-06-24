befc40bbefa97237e5994965547c9434cd2d6376
anaelisaq <anaelisa.abs@gmail.com>
2022-06-23 22:02:46 -0300

    src

---
+++ src/models/tagSchema.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose")
+
+const tagSchema = new mongoose.Schema({
+    
+})
--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -6,7 +6,9 @@ const router = express.Router()
 router.get("/all", controller.findAll)
 
 router.post("/new", controller.createNote)
+
 router.put("/update/:id", controller.updateNote)
+
 router.delete("/delete/:id", controller.deleteNote)
 
 module.exports = router
